80be6f0a47955cd77a6e6c4958c4b01bdcd474f1
jamesarthur <james_s_arthur@sbcglobal.net>
2018-02-06 13:24:13 -0600

    test logic for pm2

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  
 
-  res.render('index', { title: 'Running with PM2 again', name: 'jamesarthur'});
+  res.render('index', { title: 'Hello World', name: 'jamesarthur'});
+});
+
+//crash the sever for pm2 testing
+router.get('/exit', function(req, res, next){
+  process.exit(1);
 });
 
 module.exports = router;
